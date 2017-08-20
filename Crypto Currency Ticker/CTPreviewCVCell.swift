13f8cde89ab38230b71c27685f186ba85979837d
Matthew Riley <mriley@86borders.com>
2017-08-20 00:03:15 -0400

    -added CrytpoTicker as Pod and some coin images

--- Crypto Currency Ticker/CTPreviewCVCell.swift
+++ Crypto Currency Ticker/CTPreviewCVCell.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CrytpoTicker
+import SDWebImage
 
 class CTPreviewCVCell: UICollectionViewCell {
 
@@ -26,6 +27,42 @@ class CTPreviewCVCell: UICollectionViewCell {
         self.tickerSymbol.text = ticker.symbol
         self.cryptoName.text = ticker.name
         self.cryptoPrice.text = "$" + "\(ticker.price_usd?.doubleValue ?? 0.0)"
+        
+        if let sym = ticker.symbol {
+            if let urlString = self.urlFor(symbol: sym) {
+                iconImageView.sd_setShowActivityIndicatorView(true)
+                iconImageView.sd_setIndicatorStyle(.whiteLarge)
+                iconImageView.sd_setImage(with: URL(string: urlString), placeholderImage: #imageLiteral(resourceName: "coinPlaceholder"))
+            }else{
+                self.iconImageView.image = #imageLiteral(resourceName: "coinPlaceholder")
+            }
+        }
+        
+    }
+    
+    //used since I haven't stored many images on the website, if all were stored, then I could construct URL to retrieve image
+    func urlFor(symbol : String) -> String? {
+        
+        switch symbol {
+            case "BTC":
+            return "https://farawaystars.com/cryptoImages/BTC.png"
+            case "BCH":
+            return "https://farawaystars.com/cryptoImages/BCH.png"
+            case "ETH":
+            return "https://farawaystars.com/cryptoImages/ETH.png"
+            case "ETC":
+            return "https://farawaystars.com/cryptoImages/ETC.png"
+            case "XMR":
+            return "https://farawaystars.com/cryptoImages/XMR.png"
+            case "DASH":
+            return "https://farawaystars.com/cryptoImages/DASH.png"
+            case "ZEC":
+            return "https://farawaystars.com/cryptoImages/ZEC.png"
+            
+            
+        default:
+            return nil
+        }
     }
 
 }
